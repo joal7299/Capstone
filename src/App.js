ffc4e236b26ff25454d6dea0d5bb30cd0a80cfcd
Sophie Foster <sofo1413@colorado.edu>
2019-09-03 14:01:08 -0600

    Sophie's branch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import Lottie from 'react-lottie'
 import animationData from './lotties/Square-test.json'
+import Typed from 'typed.js';
 
 class App extends React.Component {
   render() {
-      const defaultOptions = {
-          loop: true,
-          autoplay: true,
-          animationData: animationData,
-          rendererSettings: {
-              preserveAspectRatio: 'xMidYMid slice'
-          }
-      };
+      // const defaultOptions = {
+      //     loop: true,
+      //     autoplay: true,
+      //     animationData: animationData,
+      //     rendererSettings: {
+      //         preserveAspectRatio: 'xMidYMid slice'
+      //     }
+      // };
 
     return (
         <div>
           <h1>The app!</h1>
-            <Lottie options={defaultOptions} height={400} width={400} />
+            {/*<Lottie options={defaultOptions} height={400} width={400} />*/}
         </div>
     )
   }
