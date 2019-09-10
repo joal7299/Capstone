9e400fcd09786e9c432a44390143a34dfd31eb46
Sophie Foster <sofo1413@colorado.edu>
2019-09-10 13:05:01 -0600

    Sophie recent

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Lottie from 'react-lottie'
 import animationData from './lotties/Square-test.json'
+import { Redirect } from 'react-router-dom'
 // import Typed from 'typed.js';
 import Typed from 'react-typed'
+import Button from "@material-ui/core/Button";
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            complete: false
+        };
+        this.redirect = this.redirect.bind(this);
+    }
+
+    redirect() {
+        console.log("made it here");
+        this.setState({complete:true});
+
+    }
     // componentDidMount() {
     //     const { strings } = this.props;
     //     const options = {
@@ -15,6 +30,7 @@ class App extends React.Component {
     //     };
     //     this.typed = new Typed(this.el, options);
     // }
+
   render() {
       const defaultOptions = {
           loop: true,
@@ -27,12 +43,17 @@ class App extends React.Component {
 
     return (
         <div style={{textAlign: "center"}}>
-            <Typed style={{fontSize: "3em", textAlign: "center"}} strings={['your name is Stephanie Walker', 'you are 22 years old','on September 5th, 2019, you were driving home', 'and from what seemed to be out of nowhere', 'you were hit and killed in a severe car accident', 'life would never be the same.', 'welcome to your funeral']} typeSpeed={35} backSpeed={50}/>
-            <Lottie options={defaultOptions} height={400} width={400} />
+            <Typed style={{fontSize: "3em", textAlign: "center"}} strings={['your name is Stephanie Walker', 'you are 22 years old','on September 5th, 2019, you were driving home', 'and from what seemed to be out of nowhere', 'you were hit and killed in a severe car accident', 'life would never be the same.', 'welcome to your funeral']} typeSpeed={35} backSpeed={50} onComplete={function(){this.setState({complete:true})}}
+        />
+            <Button>hello</Button>
+            {this.state.complete == &
+                <Redirect to="/Home.js"/>true
+            }
+            {/*<Lottie options={defaultOptions} height={400} width={400} />*/}
         </div>
-
-    )
+  )
   }
 }
 
 export default App;
+
