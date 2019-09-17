e17007d8b577e5339ca106fad66892aeb4122d6f
Sophie Foster <sofo1413@colorado.edu>
2019-09-17 13:18:48 -0600

    Sophie cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import Lottie from 'react-lottie'
-import animationData from './lotties/Square-test.json'
 import { Redirect } from 'react-router-dom'
-// import Typed from 'typed.js';
 import Typed from 'react-typed'
-import Button from "@material-ui/core/Button";
 
 class App extends React.Component {
     constructor(props) {
@@ -16,15 +11,6 @@ class App extends React.Component {
     }
 
   render() {
-      const defaultOptions = {
-          loop: true,
-          autoplay: true,
-          animationData: animationData,
-          rendererSettings: {
-              preserveAspectRatio: 'xMidYMid slice'
-          }
-      };
-
     return (
         <div style={{textAlign: "center"}}>
             <Typed style={{fontSize: "3em", textAlign: "center"}} strings={['your name is Stephanie Walker', 'you are 22 years old','on September 5th, 2019, you were driving home', 'and from what seemed to be out of nowhere', 'you were hit and killed in a severe car accident', 'life would never be the same.', 'welcome to your funeral']}  fadeOutDelay= {500}
@@ -33,7 +19,6 @@ class App extends React.Component {
             {this.state.complete == true &&
                 <Redirect to="/home"/>
             }
-            {/*<Lottie options={defaultOptions} height={400} width={400} />*/}
         </div>
   )
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,8 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-
-                {/*<h1>Welcome!</h1>*/}
-                {/*<img alt="initial screen" style={{ width: "100%", maxHeight: "90%", margin: "0", padding: "0"}} src={HomePic}/>*/}
-
                 <Funeral>
                     <img style={{zIndex: 1}} src={Text}/>
-
                 </Funeral>
             </div>
         )
