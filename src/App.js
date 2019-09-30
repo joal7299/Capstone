71845d5b1471b58df165b914b7f0ef319e17d2ec
Sophie Foster <sofo1413@colorado.edu>
2019-09-29 18:23:46 -0600

    Sophie test

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
 
   render() {
     return (
-        <div style={{textAlign: "center"}}>
+        <div className="typed" style={{textAlign: "center"}}>
             <Typed className="typedIntro" strings={['your name is Stephanie Walker', 'you are 22 years old','on September 5th, 2019, you were driving home', 'and from what seemed to be out of nowhere', 'you were hit and killed in a severe car accident', 'life would never be the same.', 'welcome to your funeral']}  fadeOutDelay= {500}
             typeSpeed={35} backSpeed={50} onComplete={()=>{setTimeout(function() {this.setState({complete:true})}.bind(this),
                 2000)}}
--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,10 +1,29 @@
 import React from 'react'
-
+import Lizzo from "../img/lizzo.mp3"
 class Friend extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            play: false,
+            pause: true,
+        }
+        this.audio = new Audio(Lizzo);
+        this.song = this.song.bind(this);
+    }
+
+    song = ()=> {
+        this.setState({play:true});
+        this.audio.play();
+    }
     render() {
         return (
             <div>
+            <span>
+            </span>
+            <div>
                 <h1>Friend</h1>
+                <button onClick={this.song}>Click me!</button>
+            </div>
             </div>
         )
     }
