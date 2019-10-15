a2f934568298d057bc5cebf3ef82368bead626e0
Jonathan Allen <joal7299@colorado.edu>
2019-10-15 13:24:08 -0600

    updated friend scene again

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,18 +1,65 @@
 import React from 'react'
+import Lizzo from "../img/lizzo.mp3"
+// import {ReactComponent as ProgressBar} from "../img/background.svg"
 
-import Hover from "./Hover.js"
-
-import {ReactComponent as BusStop} from "../img/table.svg"
-import {ReactComponent as Home} from "../img/leftArrow.svg"
 
 class Friend extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            play: false,
+            pause: true,
+            playbackrate: .1,
+            top: 150,
+            left: 150,
+            visible: true,
+            percentage: 0
+        }
+        this.audio = new Audio(Lizzo);
+        this.song = this.song.bind(this);
+    }
+
+    song = ()=> {
+        this.setState({top: (Math.floor(Math.random()*(window.screen.availHeight))), left: Math.floor(Math.random()*(window.screen.availWidth))});
+        console.log(this.state.left);
+        if(this.state.playbackrate<1.5) {
+            this.setState({playbackrate: this.state.playbackrate+.1});
+            this.audio.playbackRate = this.state.playbackrate;
+            this.audio.play();
+            console.log(this.state.playbackrate);
+        }
+        if(this.state.playbackrate>1.3) {
+            this.setState({visible: false});
+        }
+
+        // this.setState({play:true});
+    }
+    // const Filler = (props) => {
+    //     return <div className=="filler" />
+    // }
+    //
+    // const ProgressBar = (props) => {
+    //     return(
+    //         <div className="progress-bar"></div>
+    //         <Filler percentage={props.percentage} />
+    //     )
+    // }
+
+
     render() {
+        let top = this.state.top;
+        let left = this.state.left;
+        console.log(top);
         return (
             <div>
                 <h1>Friend</h1>
-                <Hover noHoverSvg={<BusStop className="Desk"/>} hoverSvg={<BusStop cursor="pointer" onClick={()=> window.location.href="musicGame"} className="Desk"/>}/>
-                <Hover noHoverSvg={<Home className="Home"/>} hoverSvg={<Home cursor="pointer" onClick={()=> window.location.href="Home"} className="Home"/>}/>
-            </div>
+                {this.state.visible == true &&
+                    <div>
+                        {/*<ProgressBar percentage={this.state.percentage} />*/}
+                <button style={{position: "absolute", top: top, left: left}} onClick={this.song}>Click me!</button>
+                    </div>
+                        }
+                </div>
         )
     }
 }
