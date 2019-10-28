64bc77a89b5aece6137e3ff9fd5a060015c9e6d8
Sophie Foster <sofo1413@colorado.edu>
2019-10-28 16:05:23 -0600

    sophie changes with new characters and texts

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,11 @@ import {ReactComponent as Funeral} from "../img/background.svg"
 import {ReactComponent as Gravestone} from "../img/gravestone-1.svg"
 import {ReactComponent as Grey1} from "../img/grey1.svg"
 import {ReactComponent as Person1} from "../img/person1.svg"
+import {ReactComponent as Boyfriend} from "../img/boyfriend.svg"
+import {ReactComponent as Friend} from "../img/bestfriend.svg"
+import {ReactComponent as Mom} from "../img/mom.svg"
+
+
 import Hover from "./Hover.js"
 import {ReactComponent as Grey2} from "../img/grey2.svg"
 import {ReactComponent as Person2} from "../img/person2.svg"
@@ -17,11 +22,14 @@ class Home extends React.Component {
     render() {
         return (
             <div className="container">
-                <Hover noHoverSvg={<Grey1 className="grey1"/>} hoverSvg={<Person1 onClick={()=> window.location.href="Sister"} className="person1"/>} textBox={<img src={Text} className="textBubbleGrey1" />} texts={<p className="textHomeGrey1">I miss my sister. I don’t<br></br> know what I’ll do without<br></br> her; I’ve never had to try.</p>}/>
-                <Hover noHoverSvg={<Grey2 className="grey2"/>} hoverSvg={<Person2 onClick={()=> window.location.href="Boyfriend"} className="person2"/>} textBox={<img src={Text} className="textBubbleGrey2" />} texts={<p className="textHomeGrey2">We were starting to build<br></br> our lives together. Now she’s <br></br> gone, I'm heartbroken. </p>}/>
-                <Hover noHoverSvg={<Grey3 className="grey3"/>} hoverSvg={<Person3 onClick={()=> window.location.href="Friend"} className="person3"/>} textBox={<img src={Text} className="textBubbleGrey3" />} texts={<p className="textHomeGrey3">All I see when I close my <br></br>eyes is Stephanie. I can still<br></br> hear my best friends laugh.</p>}/>
-                <Hover noHoverSvg={<Grey4 className="grey4"/>} hoverSvg={<Person4 onClick={()=> window.location.href="Driver"} className="person4"/>} textBox={<img src={Text} className="textBubbleGrey4" />} texts={<p className="textHomeGrey4">I can’t believe it, I feel so <br></br> guilty. I don’t know what I <br></br>can do to make this right.</p>}/>
-                <Hover noHoverSvg={<Grey5 className="grey5"/>} hoverSvg={<Person5 onClick={()=> window.location.href="Parent"} className="person5"/>} textBox={<img src={Text} className="textBubbleGrey5" />} texts={<p className="textHomeGrey5">My baby girl Stephanie<br></br> is gone, I just don’t know<br></br> what I’m going to do. </p>}/>
+                <Boyfriend className="grey2"/>
+                <Friend className="grey3"/>
+                <Mom className="grey4"/>
+                {/*<Hover noHoverSvg={<Grey1 className="grey1"/>} hoverSvg={<Person1 onClick={()=> window.location.href="Sister"} className="person1"/>} textBox={<img src={Text} className="textBubbleGrey1" />} texts={<p className="textHomeGrey1">I miss my sister. I don’t<br></br> know what I’ll do without<br></br> her; I’ve never had to try.</p>}/>*/}
+                {/*<Hover noHoverSvg={<Grey2 className="grey2"/>} hoverSvg={<Person2 onClick={()=> window.location.href="Boyfriend"} className="person2"/>} textBox={<img src={Text} className="textBubbleGrey2" />} texts={<p className="textHomeGrey2">We were starting to build<br></br> our lives together. Now she’s <br></br> gone, I'm heartbroken. </p>}/>*/}
+                {/*<Hover noHoverSvg={<Grey3 className="grey3"/>} hoverSvg={<Person3 onClick={()=> window.location.href="Friend"} className="person3"/>} textBox={<img src={Text} className="textBubbleGrey3" />} texts={<p className="textHomeGrey3">All I see when I close my <br></br>eyes is Stephanie. I can still<br></br> hear my best friends laugh.</p>}/>*/}
+                {/*<Hover noHoverSvg={<Grey4 className="grey4"/>} hoverSvg={<Person4 onClick={()=> window.location.href="Driver"} className="person4"/>} textBox={<img src={Text} className="textBubbleGrey4" />} texts={<p className="textHomeGrey4">I can’t believe it, I feel so <br></br> guilty. I don’t know what I <br></br>can do to make this right.</p>}/>*/}
+                {/*<Hover noHoverSvg={<Grey5 className="grey5"/>} hoverSvg={<Person5 onClick={()=> window.location.href="Parent"} className="person5"/>} textBox={<img src={Text} className="textBubbleGrey5" />} texts={<p className="textHomeGrey5">My baby girl Stephanie<br></br> is gone, I just don’t know<br></br> what I’m going to do. </p>}/>*/}
                 <Gravestone className="gravestone"/>
                 <Funeral/>
             </div>
--- src/components/TextMomtoFriend.js
+++ src/components/TextMomtoFriend.js
@@ -1,25 +1,41 @@
 import React from 'react'
 import {ReactComponent as TextFrom} from "../img/text-from.svg"
 import {ReactComponent as TextTo} from "../img/text-to.svg"
-import lax from '../../node_modules/lax.js'
-import Fade from 'react-reveal/Fade'
+import "animate.css/animate.min.css"
+import ScrollAnimation from "react-animate-on-scroll"
+import { Redirect } from 'react-router-dom'
 
 
 class TextMomtoFriend extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            complete: false
+        };
     }
 
     render() {
         return (
             <div>
-
-                <Fade right>
+                <ScrollAnimation animateIn="bounceInRight">
                     <TextTo className="textTo"></TextTo>
-                </Fade>
-                <Fade left>
+                    <p className="textFromMomtoFriend">Hey Jess, hope you're<br></br> hanging in there.</p>
+                </ScrollAnimation>
+                <ScrollAnimation delay="2600" animateIn="bounceInLeft">
                     <TextFrom className="textFrom"></TextFrom>
-                </Fade>
+                    <p className="textFromMomtoFriend2">Hi Mrs.Walker! I miss<br></br> Steph a ton, but doing what<br></br> I can to be okay for now.</p>
+                </ScrollAnimation>
+                <ScrollAnimation delay ="5500" animateIn="bounceInRight">
+                    <TextTo className="textTo"></TextTo>
+                    <p className="textFromMomtoFriend3">Same here. I found a<br></br> sweet note she wrote<br></br> about me.. really comforting</p>
+                </ScrollAnimation>
+                <ScrollAnimation delay ="6100" animateIn="bounceInRight">
+                    <TextTo className="textTo2"></TextTo>
+                    <p className="textFromMomtoFriend4">It made me think of you,<br></br>she loved you SO much.<br></br> xoxo</p>
+                </ScrollAnimation>
+                {this.state.complete == true &&
+                <Redirect to="/home"/>
+                }
             </div>
         )
     }
