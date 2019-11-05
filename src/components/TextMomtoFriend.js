eda90ef1af28ec1bd0f2a1364f19f7265f6358ef
Jonathan Allen <joal7299@colorado.edu>
2019-11-05 14:52:35 -0700

    transition test

--- src/components/TextMomtoFriend.js
+++ src/components/TextMomtoFriend.js
@@ -19,21 +19,21 @@ class TextMomtoFriend extends React.Component {
         return (
             <div>
 
-                <div id="text" className="">
-                    <TextTo className="textTo"></TextTo>
+                {/* <div id="text" className="textTo">
+                    <TextTo className=""></TextTo> */}
                     {/*<p className="textFromMomtoFriend">Hey Jess, hope you're<br></br> hanging in there.</p>*/}
-                    <p className="textFromMomtoFriend">Hey Eric, hope you're<br></br> hanging in there.</p>
-                </div>
+                    {/* <p className="textFromMomtoFriend">Hey Eric, hope you're<br></br> hanging in there.</p>
+                </div> */}
             
-                <div id="text" className="textFromMomtoFriend2">
+                <div id="text" className="sendText">
                     <TextFrom className="textFrom"></TextFrom>
                     <p className="textFromMomtoFriend2">Hi Mrs.Walker! I miss<br></br> Steph a ton, but doing what<br></br> I can to be okay for now.</p>
                 </div>
                 
-                <div>
+                {/* <div>
                     <TextTo className="textTo"></TextTo>
                     <p className="textFromMomtoFriend3">Same here. I found a sweet<br></br> note she wrote about me..<br></br> really comforting</p>
-                </div>
+                </div> */}
                 
                 {/* <div>
                     <TextTo className="textTo2"></TextTo>
